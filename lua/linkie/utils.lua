726086be350f2efa0965e543212e69d63dc13d16
omeyemburg <135001586+omeyenburg@users.noreply.github.com>
2025-03-16 21:35:23 +0100

    feat(utils): add table contains helper function

--- lua/linkie/utils.lua
+++ lua/linkie/utils.lua
@@ -2,4 +2,15 @@
 
 local M = {}
 
+---@param tbl table
+---@param value any
+function M.tbl_contains(tbl, value)
+    for _, v in ipairs(tbl) do
+        if v == value then
+            return true
+        end
+    end
+    return false
+end
+
 return M
