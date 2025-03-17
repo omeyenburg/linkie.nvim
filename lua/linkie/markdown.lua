4f5d6a288d5896def9a375dcd3b11d6ed0dd209e
omeyemburg <135001586+omeyenburg@users.noreply.github.com>
2025-03-17 17:16:34 +0100

    feat(markdown): markdown link extraction

--- lua/linkie/markdown.lua
+++ lua/linkie/markdown.lua
@@ -54,7 +54,6 @@ local function get_type_children(node)
     for child in node:iter_children() do
         local child_type = child:type()
         children[child_type] = child
-        print(child:type())
     end
 
     return children
@@ -84,32 +83,47 @@ local function get_markdown_link(children)
     local parser = vim.treesitter.get_parser()
     local tree = parser:parse()[1]
     local root = tree:root()
-
     local query = vim.treesitter.query.parse(
         'markdown',
         [[
-          (link_reference_definition
+        (link_reference_definition
             (link_label) @label
-            (link_destination) @destination)
+            (link_destination))
         ]]
     )
 
-    for id, node, _ in query:iter_captures(root, 0, 0, -1) do
-        local capture_name = query.captures[id]
-        local capture_text = vim.treesitter.get_node_text(node, 0):lower()
-
-        if capture_name == 'label' then
-            local destination_node = node:next_named_sibling()
-            if destination_node and capture_text == label_text then
-                return get_node_text(destination_node)
-            end
+    for _, node, _ in query:iter_captures(root, 0, 0, -1) do
+        local destination_node = node:next_named_sibling()
+        if destination_node and get_node_text(node):lower() == label_text then
+            return get_node_text(destination_node)
         end
     end
 
     return nil
 end
 
+---@return table anchors
+local function get_anchors()
+    local anchors = {}
+
+    local parser = vim.treesitter.get_parser(0, 'markdown')
+    local tree = parser:parse()[1]
+    local root = tree:root()
+    local query = vim.treesitter.query.parse('markdown', '(atx_heading (inline) @content)')
+
+    for _, node, _ in query:iter_captures(root, 0, 0, -1) do
+        local line = node:start()
+        local heading = get_node_text(node)
+
+        anchors[line] = heading:gsub(' ', '-'):lower()
+    end
+
+    return anchors
+end
+
 ---@param markdown_link string
+---@return "line"|"uri"|"path"|"none" type
+---@return integer|string destination
 local function get_markdown_destination(markdown_link)
     --[[
     Example links:
@@ -121,47 +135,67 @@ local function get_markdown_destination(markdown_link)
     [/page/#conclusion]         -> ./page/ -> # Conclusion
     [https://en.wikipedia.org/] -> https://en.wikipedia.org/
     [file.///path]              -> /path
-
     ]]
+
+    local beginning = markdown_link:sub(1, 1)
+
+    if beginning == '#' then
+        -- For Obsidian we also substitute spaces with hyphens
+        -- to allow non-standard links with spaces.
+        local anchor = markdown_link:sub(2):lower():gsub(' ', '-')
+        local anchors = get_anchors()
+        for line, a in pairs(anchors) do
+            if anchor == a then
+                return 'line', line
+            end
+        end
+
+        return 'none', 0
+    elseif Utils.validate_uri(markdown_link) then
+        return 'uri', markdown_link
+    else
+        return 'path', markdown_link
+    end
 end
 
 local M = {}
 
 ---@param node TSNode?
+---@return "line"|"uri"|"email"|"path"|"none" type
+---@return integer|string destination
 function M.handle_markdown_node(node)
     -- We need to find the outer parent link node.
     -- Depending on the context, node could be one of a variety of unrelated child nodes:
     -- text, backslash_escape, character_reference, emphasis, etc.
     node = get_link_node(node)
     if node == nil then
-        return nil
+        return 'none', 0
     end
 
     local node_type = node:type()
 
     -- Return autolinks early
     if node_type == 'email_autolink' then
-        return {
-            type = 'email',
-            text = strip_autolink(get_node_text(node)),
-        }
+        return 'email', strip_autolink(get_node_text(node))
     end
     if node_type == 'uri_autolink' or node_type == 'www_autolink' then
-        return {
-            type = 'link',
-            text = strip_autolink(get_node_text(node)),
-        }
+        return 'uri', strip_autolink(get_node_text(node))
     end
 
     local children = get_type_children(node)
 
     local markdown_link = get_markdown_link(children)
     if markdown_link == nil then
-        return nil
+        -- For Obsidian we also cover links that are not
+        -- possible in standard markdown, such as [[#section]]
+        if children.link_text and get_node_text(children.link_text):sub(1, 1) == '#' then
+            markdown_link = get_node_text(children.link_text)
+        else
+            return 'none', 0
+        end
     end
-    print('markdown link: ' .. markdown_link)
 
-    get_markdown_destination(markdown_link)
+    return get_markdown_destination(markdown_link)
 end
 
 return M
